c0ea90cb67063e0c6e23eb1495ffc244c1df0851
DESKTOP-HMLJOE7\legion <kudashevtim@gmail.com>
2025-02-12 17:14:56 +0300

    news

---
+++ src/img/html_news/index.php
@@ -0,0 +1,10 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetPageProperty("title", "Кондитерская Олега Ильина");
+$APPLICATION->SetPageProperty("description", "Информация о кондитерской Олега Ильина.");
+$APPLICATION->SetTitle("О нас");
+?>
+
+
+
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
